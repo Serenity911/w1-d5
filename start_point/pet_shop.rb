f855d17688203523588877bb950eb7f640d3d763
Serenity911 <silvia.simonassi01@gmail.com>
2019-11-22 14:53:39 +0000

    added function to check if customer can afford pet

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -62,3 +62,15 @@ end
 def remove_customer_cash(customer, cash)
   customer[:cash] -= cash
 end
+
+def customer_pet_count(customer)
+    return customer[:pets].length
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets] << new_pet
+end
+
+def customer_can_afford_pet(customer, new_pet)
+  customer[:cash] >= new_pet[:price] ? true : false
+end
